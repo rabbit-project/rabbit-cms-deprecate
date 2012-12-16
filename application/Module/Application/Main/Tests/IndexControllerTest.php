ccc08c43b3dd50430f3a5d1a8584df8b1114b0ae
Erick Leao <erickleao@gmail.com>
2012-12-16 17:20:52 -0200

    Configurando phpunit

---
+++ application/Module/Application/Main/Tests/IndexControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Application\Tests;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController extends \PHPUnit_Framework_TestCase {
+		
+	public function testExempleIsEmpty() {
+		$this->assertEmpty("");
+	}
+	
+}
